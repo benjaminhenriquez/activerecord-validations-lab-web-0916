ae2658771a75ed1b9c48d4b6db0c1e1c572b4b8f
github username <github email address>
2016-10-26 10:50:44 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,31 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :clickbaity?
+
+
+
+
+    def clickbaity?
+      top_num = (1..9).map {|i| "Top #{i}"}
+      array = ["Won't Believe", "Secret", "Guess", "True"] + top_num
+
+      if self.title
+        bait_bool = array.any? do |phrase|
+          self.title.include?(phrase)
+        end
+        if bait_bool
+          return true
+        else
+          return false
+        end
+      else
+        false
+      end
+    end
+
+
 end
